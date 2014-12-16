1bc9ccfc8c57d6b77d94ffa2fe97e95c8e980ae3
Allen Zhao <cnallenzhao@gmail.com>
2014-12-16 15:59:07 +0800

    Make dirs for controllers and models, clean up some codes

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
 // Get the packages we need
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var studentController = require('./controller/student');
+    // Connect to mongodb
+mongoose.connect("mongodb://localhost:27017/api-dev");
 
 // Create our Express application
 var app = express();
 
+
+
 // Use environment defined port or 3000
 var port = process.env.PORT || 3000;
+
+
 
 // Create our Express router
 var router = express.Router();
 
-// Initial dummy route for testing
-// http://localhost:3000/api
-router.get('/', function(req, res) {
-  res.json({ message: 'Hello, API server running now.' });
-});
+router.route('/students')
+	.post(studentController.postStudents)
+	.get(studentController.getStudents);
+
+router.route('/students/:studentId')
+	.get(studentController.getStudent)
+	.put(studentController.putStudent)
+	.delete(studentController.deleteStudent);
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // Register all our routes with /api
 app.use('/api', router);
 
 // Start the server
 app.listen(port);
-console.log('Insert beer on port ' + port);
+console.log('API server running on ' + port);
